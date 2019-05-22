25bcee7177672348d85cedc86021a304ecfcd639
Gregory Johnson <gaj3@illinois.edu>
2019-05-22 18:12:57 -0500

    organize code

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
-int main(int argc, char **argv) {
-	SDL_Window *window = NULL;
-	SDL_Surface *surface = NULL;
+
+int init() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL init error: %s\n", SDL_GetError());
-		goto exit;
+		return -1;
 	}
+	return 0;
+}
 
-	window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+SDL_Window* create_window(char *name, int width, int height) {
+	SDL_Window *window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
 	if (window == NULL) {
 		printf("SDL create window error: %s\n", SDL_GetError());
-		goto exit;
+		return NULL;
 	}
-	surface = SDL_GetWindowSurface(window);
+	return window;
+}
+
+int main(int argc, char **argv) {
+	init();
+	SDL_Window *window = create_window("test", 640, 480);
+	SDL_Surface *surface = SDL_GetWindowSurface(window);
+
 	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0xFF, 0xFF));
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(2000);
 
-exit:
+	// clean up
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
