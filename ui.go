26362d0d0eff40f5bf497ea7c959dc39abd6a93e
Tyler Kropp <kropptyler@gmail.com>
2019-05-31 15:50:34 -0400

    Make UIComponent events more specific

--- ui.go
+++ ui.go
@@ -1,12 +1,14 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 // UIComponent says what functions a UIComponent must implement
 type UIComponent interface {
 	getBoundary() *sdl.Rect
 	render() (*sdl.Texture, error)
-	onEnter(*sdl.Event)
-	onLeave(*sdl.Event)
-	onClick(*sdl.Event)
+	onEnter(*sdl.MouseMotionEvent)
+	onLeave(*sdl.MouseMotionEvent)
+	onClick(*sdl.MouseButtonEvent)
 }
