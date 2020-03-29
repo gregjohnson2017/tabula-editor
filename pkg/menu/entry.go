861d3a72c4249957d0e86557e6937c1cab9f2dce
Tyler Kropp <kropptyler@gmail.com>
2020-03-29 00:25:54 -0400

    Add main refactor

--- pkg/menu/entry.go
+++ pkg/menu/entry.go
@@ -1,125 +1,106 @@
 package menu
 
 import (
-	"fmt"
-
 	"github.com/gregjohnson2017/tabula-editor/pkg/config"
 	"github.com/gregjohnson2017/tabula-editor/pkg/ui"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-// MenuEntry is the clickable entry which opens a MenuList
-type MenuEntry struct {
+// entry is the clickable entry which opens a list
+type entry struct {
 	enabled bool
-	button  *MenuBarButton
-	list    *MenuList
-	action  func()
+	button  *listButton
+	list    *list
 }
 
-// MenuBarButton defines an interactive button, but redefines OnClick to perform action on press, not release
-type MenuBarButton struct {
-	*Button
-}
+// newEntry returns the struct with the given label and list
+func newEntry(cfg *config.Config, area *sdl.Rect, label string, align ui.Align, menus []Definition, act func()) (*entry, error) {
+	btn, err := NewButton(area, cfg, label, act)
+	if err != nil {
+		return nil, err
+	}
 
-// OnClick is called when the user clicks within the UIComponent's region
-func (mbb *MenuBarButton) OnClick(evt *sdl.MouseButtonEvent) bool {
-	if evt.Button == sdl.BUTTON_LEFT && evt.State == sdl.PRESSED {
-		mbb.pressed = true
-		mbb.action()
-	} else if evt.Button == sdl.BUTTON_LEFT && evt.State == sdl.RELEASED && mbb.pressed {
-		mbb.pressed = false
-		// TODO add release action
+	pos := sdl.Point{area.X, area.Y}
+	if align.H == ui.AlignRight {
+		pos.X += area.W
 	}
-	return true
-}
+	if align.V == ui.AlignBelow {
+		pos.Y += area.H
+	}
+	list, err := newList(cfg, pos, menus)
+	if err != nil {
+		return nil, err
+	}
 
-// NewMenuEntry returns the struct with the given label and list
-func NewMenuEntry(area *sdl.Rect, label string, list *MenuList, cfg *config.Config, act func()) (MenuEntry, error) {
-	if cfg == nil {
-		return MenuEntry{}, fmt.Errorf("NewMenuEntry found nil Config")
-	}
-	if list == nil && act == nil {
-		return MenuEntry{}, fmt.Errorf("NewMenuEntry needs a list and/or an action")
-	}
-	var btn *Button
-	var err error
-	if btn, err = NewButton(area, cfg, label, act); err != nil {
-		return MenuEntry{}, err
-	}
-	return MenuEntry{
+	return &entry{
 		enabled: false,
-		button:  &MenuBarButton{btn},
+		button:  &listButton{*btn},
 		list:    list,
 	}, nil
 }
 
-// Destroy calls destroy on underlying UIComponents
-func (me MenuEntry) Destroy() {
-	me.button.Destroy()
-	me.list.Destroy()
+// Destroy calls destroy on underlying ui.Components
+func (e *entry) Destroy() {
+	e.button.Destroy()
+	e.list.Destroy()
 }
 
-// InBoundary returns whether a point is in this UIComponent's bounds
-func (me MenuEntry) InBoundary(pt sdl.Point) bool {
-	if me.button.InBoundary(pt) {
+// InBoundary returns whether a point is in this ui.Component's bounds
+func (e *entry) InBoundary(pt sdl.Point) bool {
+	if e.button.InBoundary(pt) {
 		return true
 	}
-	if me.enabled && me.list != nil && me.list.InBoundary(pt) {
+	if e.enabled && e.list != nil && e.list.InBoundary(pt) {
 		return true
 	}
 	return false
 }
 
-// GetBoundary returns the underlying button's boundary
-func (me MenuEntry) GetBoundary() *sdl.Rect {
-	return me.button.GetBoundary()
-}
-
 // OnEnter calls the underlying button's OnEnter method
-func (me *MenuEntry) OnEnter() {
-	me.button.OnEnter()
+func (e *entry) OnEnter() {
+	e.button.OnEnter()
 }
 
 // OnLeave calls the underlying button's OnLeave method
-func (me *MenuEntry) OnLeave() {
-	me.button.OnLeave()
-	me.list.OnLeave()
+func (e *entry) OnLeave() {
+	e.button.OnLeave()
+	e.list.OnLeave()
 }
 
 // OnClick calls the underlying button's OnClick method
-func (me *MenuEntry) OnClick(evt *sdl.MouseButtonEvent) bool {
+func (e *entry) OnClick(evt *sdl.MouseButtonEvent) bool {
 	if evt.Button != sdl.BUTTON_LEFT || evt.State != sdl.PRESSED {
 		return true
 	}
-	if ui.InBounds(*me.GetBoundary(), sdl.Point{evt.X, evt.Y}) {
-		me.button.OnClick(evt)
-		me.enabled = !me.enabled
+	if e.button.InBoundary(sdl.Point{X: evt.X, Y: evt.Y}) {
+		e.button.OnClick(evt)
+		e.enabled = !e.enabled
 		return true
 	}
-	if e, err := me.list.GetEntryAt(evt.X, evt.Y); err == nil {
+	if e, err := e.list.GetEntryAt(evt.X, evt.Y); err == nil {
 		e.OnClick(evt)
 	}
 	return true
 }
 
 // Render calls the underlying button's render function
-func (me MenuEntry) Render() {
-	me.button.Render()
-	if me.list != nil && me.enabled {
-		me.list.Render()
+func (e *entry) Render() {
+	e.button.Render()
+	if e.list != nil && e.enabled {
+		e.list.Render()
 	}
 }
 
-// OnResize calls the underlying UIComponents' OnResize function
-func (me MenuEntry) OnResize(x, y int32) {
-	me.button.OnResize(x, y)
-	me.list.OnResize(x, y)
+// OnResize calls the underlying ui.Components' OnResize function
+func (e *entry) OnResize(x, y int32) {
+	e.button.OnResize(x, y)
+	e.list.OnResize(x, y)
 }
 
-// OnMotion is called when the cursor moves within the UIComponent's region - bad comment
-func (me *MenuEntry) OnMotion(evt *sdl.MouseMotionEvent) bool {
-	if me.list.InBoundary(sdl.Point{X: evt.X, Y: evt.Y}) {
-		me.list.OnMotion(evt)
+// OnMotion is called when the cursor moves within the ui.Component's region - bad comment
+func (e *entry) OnMotion(evt *sdl.MouseMotionEvent) bool {
+	if e.list.InBoundary(sdl.Point{X: evt.X, Y: evt.Y}) {
+		e.list.OnMotion(evt)
 	}
 	return true
 }
---
+++ pkg/menu/menu.go
@@ -0,0 +1,12 @@
+// Package menu provides menu-related structs for use in tabula. Specifically,
+// the package provides Bar for a menu bar implementation and List for a menu
+// entry container implementation.
+// Bar and List satisfy the tabula.ui.Component interface.
+package menu
+
+// Definition defines menu bar menus
+type Definition struct {
+	Text     string
+	Children []Definition
+	Action   func()
+}
