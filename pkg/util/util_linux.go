6a34934d4f1911360e4bc5ae8d3a76e186efb43d
Tyler Kropp <tyler@tylerkropp.xyz>
2020-06-07 01:12:04 -0400

    Add SaveFileDialog to Linux utils

--- pkg/util/util_linux.go
+++ pkg/util/util_linux.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"fmt"
+
 	"github.com/jcmuller/gozenity"
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -12,4 +14,18 @@ func OpenFileDialog(win *sdl.Window) (string, error) {
 		panic(err)
 	}
 	return files[0], nil
+}
+
+// SaveFileDialog uses a system file picker to get a file path from the user for the purpose of saving an image
+func SaveFileDialog(*sdl.Window) (string, error) {
+	folders, err := gozenity.DirectorySelection("Choose a folder to save in")
+	if err != nil {
+		return "", fmt.Errorf("SaveFileDialog: %w", err)
+	}
+	file, err := gozenity.Entry("Enter a name for the picture", "")
+	if err != nil {
+		return "", fmt.Errorf("SaveFileDialog: %w", err)
+	}
+	fmt.Printf("SaveFileDialog got back with %v folders and \"%v\" file name", folders, file)
+	return folders[0] + "/" + file, nil
 }
