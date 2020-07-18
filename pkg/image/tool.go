bb5656e5a2a5c32b04f222d0273c5699695e0b49
Tyler Kropp <tyler@tylerkropp.xyz>
2020-07-17 22:36:59 -0400

    Add Stringer to image.Tool interface

--- pkg/image/tool.go
+++ pkg/image/tool.go
@@ -1,6 +1,8 @@
 package image
 
 import (
+	"fmt"
+
 	"github.com/gregjohnson2017/tabula-editor/pkg/log"
 	"github.com/gregjohnson2017/tabula-editor/pkg/ui"
 	"github.com/veandco/go-sdl2/sdl"
@@ -10,11 +12,13 @@ import (
 type Tool interface {
 	OnClick(evt *sdl.MouseButtonEvent, iv *View)
 	OnMotion(evt *sdl.MouseMotionEvent, iv *View)
+	fmt.Stringer
 }
 
 // Make sure the tools satisfy the interface
 var _ Tool = Tool(EmptyTool{})
 var _ Tool = Tool(PixelSelectionTool{})
+var _ Tool = Tool(PixelColorTool{})
 
 // EmptyTool does nothing
 type EmptyTool struct {
@@ -26,6 +30,10 @@ func (t EmptyTool) OnClick(evt *sdl.MouseButtonEvent, iv *View) {
 
 // OnMotion does nothing
 func (t EmptyTool) OnMotion(evt *sdl.MouseMotionEvent, iv *View) {
+}
+
+func (t EmptyTool) String() string {
+	return "image.EmptyTool"
 }
 
 // PixelSelectionTool selects any clicked pixels
@@ -51,6 +59,10 @@ func (t PixelSelectionTool) OnMotion(evt *sdl.MouseMotionEvent, iv *View) {
 	}
 }
 
+func (t PixelSelectionTool) String() string {
+	return "image.PixelSelectionTool"
+}
+
 // PixelColorTool colors any clicked pixels purple
 type PixelColorTool struct {
 }
@@ -69,4 +81,8 @@ func (t PixelColorTool) OnMotion(evt *sdl.MouseMotionEvent, iv *View) {
 	if evt.State == sdl.ButtonLMask() && ui.InBounds(*iv.canvas, sdl.Point{X: evt.X, Y: evt.Y}) {
 		iv.setPixel(iv.mousePix.X, iv.mousePix.Y, []byte{0xff, 0x00, 0xff, 0xff})
 	}
+}
+
+func (t PixelColorTool) String() string {
+	return "image.PixelColorTool"
 }
